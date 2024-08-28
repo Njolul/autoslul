07abe75ab1e810cf10a4dbcd45f608cc9c81f970
Njolul <alexanderolvea@gmail.com>
2024-08-27 23:09:43 -0400

    1

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'autoslul',
+  webDir: 'www'
+};
+
+export default config;
