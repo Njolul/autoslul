a21eb3cb39556709b8b631acee6c9c78719016e4
Njolul <alexanderolvea@gmail.com>
2024-09-03 01:36:00 -0400

    act2

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  usuarioCorreo: string = "";
+  usuarioContrasena: string = "";
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+
+  }
+  validarCredenciales(){
+
+    this.router.navigate(['/inicio'],{
+      state: {
+        correo: this.usuarioCorreo,
+        contrasena: this.usuarioContrasena
+      }
+    });
+  }
+
+  
+}
